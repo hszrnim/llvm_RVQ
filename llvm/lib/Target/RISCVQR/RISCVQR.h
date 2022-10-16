bf4b68a23c0215266dffcecc7b1d06361fd2ad42
yin0224ch <yin0224ch@gmail.com>
2022-10-16 23:32:41 +0800

    dev

---
+++ llvm/lib/Target/RISCVQR/RISCVQR.h
@@ -0,0 +1,5 @@
+#ifndef LLVM_LTB_TARGET_RISCVQR_RISCVQR_H
+#define LLVM_LTB_TARGET_RISCVQR_RISCVQR_H
+
+#include "MCTargetDesc/RISCVBaseInfo.h"
+#include "llvm/Target/TargetMachine.h"
---
+++ llvm/lib/Target/RISCVQR/RISCVQRTargetMachine.h
@@ -0,0 +1,9 @@
+#ifndef LLVM_LIB_TARGET_RISCVQR_RISCVQRTARGETMACHINE_H
+#define LLVM_LIB_TARGET_RISCVQR_RISCVQRTARGETMACHINE_H
+
+#include "RISCVQRSubtarget.h"
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/Support/CodeGen.h"
+#include "llvm/Target/TargetMachine.h"
+#include <memory>
